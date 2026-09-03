ac7072d2d8740303d80aa47009c3871444b23495
puja-codes18 <pujarai12382@gmail.com>
2026-09-03 23:12:05 +0530

    Implement basic student management system

---
+++ student_management.py
@@ -0,0 +1,12 @@
+print("===== Student Management System =====")
+
+name = input("Student ka naam: ")
+age = int(input("Student ki age: "))
+course = input("Student ka course: ")
+marks = float(input("Student ke marks: "))
+
+print("\n===== Student Details =====")
+print("Name:", name)
+print("Age:", age)
+print("Course:", course)
+print("Marks:", marks)
