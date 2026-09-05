0ad471d1c88aa5f57257336660f293c08991d383
puja-codes18 <pujarai12382@gmail.com>
2026-09-05 19:33:24 +0530

    Implement student management features with menu

--- student_management.py
+++ student_management.py
@@ -1,12 +1,64 @@
 print("===== Student Management System =====")
 
-name = input("Student ka naam: ")
-age = int(input("Student ki age: "))
-course = input("Student ka course: ")
-marks = float(input("Student ke marks: "))
+students = []
 
-print("\n===== Student Details =====")
-print("Name:", name)
-print("Age:", age)
-print("Course:", course)
-print("Marks:", marks)
+while True:
+    print("\n1. Add Student")
+    print("2. Show Students")
+    print("3. Exit")
+
+    choice = input("Enter your choice: ")
+
+    if choice == "1":
+        name = input("Student ka naam: ")
+        age = int(input("Student ki age: "))
+        course = input("Student ka course: ")
+        marks = float(input("Student ke marks: "))
+
+        percentage = marks
+
+        if percentage >= 90:
+            grade = "A+"
+        elif percentage >= 80:
+            grade = "A"
+        elif percentage >= 70:
+            grade = "B"
+        elif percentage >= 60:
+            grade = "C"
+        elif percentage >= 50:
+            grade = "D"
+        else:
+            grade = "F"
+
+        student = {
+            "name": name,
+            "age": age,
+            "course": course,
+            "marks": marks,
+            "percentage": percentage,
+            "grade": grade
+        }
+
+        students.append(student)
+        print("\nStudent successfully added!")
+
+    elif choice == "2":
+        if len(students) == 0:
+            print("\nNo students found.")
+        else:
+            print("\n===== Student Details =====")
+
+            for student in students:
+                print("\nName:", student["name"])
+                print("Age:", student["age"])
+                print("Course:", student["course"])
+                print("Marks:", student["marks"])
+                print("Percentage:", student["percentage"], "%")
+                print("Grade:", student["grade"])
+
+    elif choice == "3":
+        print("Thank you!")
+        break
+
+    else:
+        print("Invalid choice!")
