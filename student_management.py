16ed6755c733526a1d2af4d94b1b49dbdf9b0ed0
puja-codes18 <pujarai12382@gmail.com>
2026-09-05 20:02:43 +0530

    Add search and delete functionality for students

--- student_management.py
+++ student_management.py
@@ -5,10 +5,13 @@ students = []
 while True:
     print("\n1. Add Student")
     print("2. Show Students")
-    print("3. Exit")
+    print("3. Search Student")
+    print("4. Delete Student")
+    print("5. Exit")
 
     choice = input("Enter your choice: ")
 
+    # Add Student
     if choice == "1":
         name = input("Student ka naam: ")
         age = int(input("Student ki age: "))
@@ -40,13 +43,15 @@ while True:
         }
 
         students.append(student)
+
         print("\nStudent successfully added!")
 
+    # Show Students
     elif choice == "2":
         if len(students) == 0:
             print("\nNo students found.")
         else:
-            print("\n===== Student Details =====")
+            print("\n===== All Students =====")
 
             for student in students:
                 print("\nName:", student["name"])
@@ -56,9 +61,46 @@ while True:
                 print("Percentage:", student["percentage"], "%")
                 print("Grade:", student["grade"])
 
+    # Search Student
     elif choice == "3":
-        print("Thank you!")
+        search_name = input("Student ka naam search karo: ")
+        found = False
+
+        for student in students:
+            if student["name"].lower() == search_name.lower():
+                print("\n===== Student Found =====")
+                print("Name:", student["name"])
+                print("Age:", student["age"])
+                print("Course:", student["course"])
+                print("Marks:", student["marks"])
+                print("Percentage:", student["percentage"], "%")
+                print("Grade:", student["grade"])
+
+                found = True
+                break
+
+        if found == False:
+            print("\nStudent not found.")
+
+    # Delete Student
+    elif choice == "4":
+        delete_name = input("Student ka naam delete karo: ")
+        found = False
+
+        for student in students:
+            if student["name"].lower() == delete_name.lower():
+                students.remove(student)
+                print("\nStudent successfully deleted!")
+                found = True
+                break
+
+        if found == False:
+            print("\nStudent not found.")
+
+    # Exit
+    elif choice == "5":
+        print("\nThank you!")
         break
 
     else:
-        print("Invalid choice!")
+        print("\nInvalid choice!")
